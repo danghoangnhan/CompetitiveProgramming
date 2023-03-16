5fbaebfbdd2d7d27caf4558250d11ac10dd45d88
danieldu <danghoangnhan.1@gmail.com>
2023-03-16 15:13:44 +0800

    complete problem3

---
+++ ITSA/202303/Problem3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+
+int main(){
+    int time,sample,result,bonus;
+    bool extra;
+    cin>>time;
+    
+    while (time--)
+    {
+        result=70;
+        cin>>sample;
+        int bonus = sample - 1500;
+        cout<<"bonus:"<<bonus<<endl;
+        if (bonus>0)
+        {   
+            if(bonus<500)bonus = 500;
+            result += (bonus/500)*5;
+            result += (bonus%500)*5;
+        }
+        cout <<result<<endl;
+    }
+    return 0;
+}
