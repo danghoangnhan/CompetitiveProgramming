8eb6f231d991a3544a70480e0b53eb64be1874bd
danieldu <danghoangnhan.1@gmail.com>
2023-03-16 20:15:21 +0800

    complele 7

---
+++ ITSA/202303/Problem7.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int population[122][3];
+int main(){
+    population[0][0]=1;
+    population[0][1]=0;
+    population[0][2]=0;
+
+    int k;
+    for (int i = 1; i <= 122; i++)
+    {
+        population[i][0]=population[i-1][1]+population[i-1][2];
+        population[i][1]=population[i-1][0];
+        population[i][2]=population[i-1][1];
+    }
+    while(cin>>k){
+        if (k>=122)
+        {
+            break;
+        }
+        cout<<population[k][0]<<endl;
+    }
+
+    return 0;
+}
