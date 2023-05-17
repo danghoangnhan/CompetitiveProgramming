8e91b876c9d5d0eedac6d63b561c9a10e0719e0f
danieldu <danghoangnhan.1@gmail.com>
2023-05-17 18:06:32 +0800

    fix the logic size

--- ITSA/202305/Problem2.cpp
+++ ITSA/202305/Problem2.cpp
@@ -12,26 +12,28 @@ using namespace std;
 #include <algorithm>
 using namespace std;
 
-int minimumDifference(int k, const vector<int>& values) {
+int minimumDifference(const vector<int>& values) {
     int totalSum = 0;
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < values.size(); i++) {
         totalSum += values[i];
     }
 
     int maxSum = totalSum / 2;
+    //dividing totalSum by 2,  for a balanced division
 
-    vector<vector<int>> dp(k + 1, vector<int>(maxSum + 1, 0));
-    for (int i = 1; i <= k; i++) {
+    vector<vector<int>> dp(values.size() + 1, vector<int>(maxSum + 1, 0));
+    
+    for (int i = 1; i <= values.size(); i++) {
         for (int j = 0; j <= maxSum; j++) {
             if (j >= values[i - 1]) {
+                //Check if the current item values[i - 1] can be included in the current total value j:
                 dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - values[i - 1]] + values[i - 1]);
             } else {
                 dp[i][j] = dp[i - 1][j];
             }
         }
     }
-
-    int minDifference = totalSum - 2 * dp[k][maxSum];
+    int minDifference = totalSum - 2 * dp[values.size()][maxSum];
     return minDifference;
 }
 
@@ -46,7 +48,7 @@ int main() {
         cin >> values[i];
     }
 
-    int result = minimumDifference(k, values);
+    int result = minimumDifference(values);
     cout << result << endl;
 
     return 0;
